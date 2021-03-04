a4f6a5be5f032743cf81b5ceba09efa6cc7727ae
Sammy Fung <sammy@sammy.hk>
2021-03-04 22:29:40 +0800

    remove unused lines from collect_users.py

--- collect_users.py
+++ collect_users.py
@@ -4,10 +4,6 @@ from datetime import datetime, timedelta
 
 def next_api_access(gh):
     now = datetime.now()
-    #next_hour = now + timedelta(hours=1)
-    #next_hour = next_hour.replace(minute=0,second=0)
-    #print('Sleep until %s' % next_hour)
-    #return (next_hour - now).total_seconds()
     next_hour = datetime.fromtimestamp(gh.rate_limiting_resettime)
     seconds = (next_hour - now).total_seconds() + 1
     print('Sleep until %s (%s sec)' % (next_hour, seconds))
